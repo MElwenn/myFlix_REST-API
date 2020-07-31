90540a7f215e6bb92e749cc796616cd3b03b20c5
MElwenn <63125829+MElwenn@users.noreply.github.com>
2020-07-31 16:49:35 +0200

    started from scratch and installed all missing modules including module method-override

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express'); // Import express package
-const morgan = require('morgan'); // logging middleware
 const app = express(); // variable that encapsulates Express’s functionality to configure the web server
-
+const morgan = require('morgan'); // logging middleware
 // Obsolete as logging is done by Morgan middleware
 //let myLogger = (req, res, next) => {
 //  console.log(req.url);
@@ -15,16 +14,16 @@ let requestTime = (req, res, next) => {
 };
 
 // middleware components
+app.use(express.static('public')); // invoke files from the public folder
 app.use(morgan('common'));         // logging
 //app.use(myLogger);               // presumably replaced by morgan
-app.use(express.static('public')); // invoke files from the public folder
 app.use(requestTime);
 
 // GET requests
-//app.get('/', (req, res) => {
+app.get('/', (req, res) => {
 //  res.send('Welcome to my movie app!');
 //});
-app.get('/', function (req, res) => {
+//app.get('/', function (req, res) => {
   let responseText = 'Welcome to my movie app!';
   responseText += '<small>Requested at: ' + req.requestTime + '</small>';
   res.send(responseText);
@@ -43,12 +42,6 @@ app.get('/documentation', (req, res) => {
   res.sendFile('public/documentation.html', { root: __dirname });
 });
 
-// error handling middleware function using express
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Internal Server ERROR occured!');
-});
-
 const bodyParser = require('body-parser'),
   methodOverride = require('method-override');
 
@@ -59,8 +52,10 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 app.use(methodOverride());
 
+// error handling middleware function using express
 app.use((err, req, res, next) => {
-  // more logic to be implemented later?
+  console.error(err.stack);
+  res.status(500).send('Internal Server ERROR occured!');
 });
 
 // JSON object containing data about your top 10 movies
@@ -119,5 +114,5 @@ let topMovies = [
 
 // listen for requests
 app.listen(8080, () =>
-  console.log('My app is listening on port 8080.');
-);
+  console.log('My app is listening on port 8080.'));
+//);
