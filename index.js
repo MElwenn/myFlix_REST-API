785af0309e4ad68ce7f60f921d7f4f1b8359a33e
MElwenn <63125829+MElwenn@users.noreply.github.com>
2020-10-01 19:51:28 +0200

    unstringed server connection in index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const mongoose = require('mongoose');
 //mongoose.connect('mongodb://localhost:27017/test', {
 //  useNewUrlParser: true, useUnifiedTopology: true
 //});
-mongoose.connect('process.env.CONNECTION_URI', {
+mongoose.connect(process.env.CONNECTION_URI, {
   useNewUrlParser: true, useUnifiedTopology: true
 });
 
