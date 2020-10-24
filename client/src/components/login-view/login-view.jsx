fe4312d176db4fd5f5be0120ddffe2df29a86084
MElwenn <63125829+MElwenn@users.noreply.github.com>
2020-10-24 11:49:33 +0200

    created ui-elements using react-bootstrap, added styling

--- client/src/components/login-view/login-view.jsx
+++ client/src/components/login-view/login-view.jsx
@@ -9,54 +9,56 @@ import Button from 'react-bootstrap/Button';
 
 import './login-view.scss';
 
-export function LoginView(props) {
+export function LoginView(props) {  //function component with hooks
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    //console.log(username, password);
-    axios.post('https://movie-api-elwen.herokuapp.com/', {
-      Username: username,
-      Password: password
-    })
-      .then((response) => {
-        const data = response.data;
-        props.onLoggedIn(data);
-      })
-      .catch((e) => {
-        console.log('User is unknown.');
-      });
-    /* Send a request to the server for authentication then call props.onLoggedIn(username)*/
-    //props.onLoggedIn(username);
+
+    console.log(username, password);
+    /* Send a request to the server for authentication */
+    /* then call props.onLoggedIn(username) */
+    props.onLoggedIn(username);
   };
+
+  // do I have to wrap 'render() {}' around the 'return()' ?
+  return (
+    <Container className="container-box">
+      <Form>
+        <Form.Group controlId="formBasicUsername">
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Firtstname blank Lastname"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <Form.Text className="text-muted">
+            We'll never share your personal data with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+
+        <Button className="button-primary" variant="primary" type="submit" onClick={this.handleSubmit}>
+          LOGIN
+        </Button>
+        <Link to={'/registration'}>
+          <Button className="button-secondary">SIGN UP</Button>
+        </Link>
+      </Form>
+    </Container>
+  );
 };
-
-return (
-  <Container>
-    <Form>
-      <Form.Group controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-    </Form.Text>
-      </Form.Group>
-
-      <Form.Group controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        LOGIN
-      </Button>
-    </Form>
-  </Container>
-);
-
 /*LoginView.propTypes = {  // why LoginView is yellow whereas in other components it's cyan?
   user: PropTypes.shape({
     Username: PropTypes.string,
--- client/src/components/registration-view/registration-view.jsx
+++ client/src/components/registration-view/registration-view.jsx
@@ -1,60 +1,82 @@
 import React, { useState } from 'react';
+//import PropTypes from 'prop-types';
 import axios from 'axios';
-import PropTypes from 'prop-types';
-import { response } from 'express';
 
-export function RegistrationView() {
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import { Link } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+
+export function RegistrationView() {  //function component with hooks
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [email, setEmail] = useState('');
   const [birthdate, setBirthdate] = useState('');
 
-  const handleSubmit = (e) => {
+  const handleSubmit = (e) => {  //Send a request to the server for authentication
     e.preventDefault();
 
-    const createdUser = {
-      Username: username,
-      Password: password,
-      Email: email,
-      Birthdate: birthdate
-    }
+    console.log(username, password);
+    /* Send a request to the server for authentication */
+    /* then call props.onLoggedIn(username) */
+    props.onLoggedIn(username);
+
   };
 
-  axios.post('<https://movie-api-elwen.herokuapp.com/users>')
-    .then((response) => {
-      console.log(response);
-      console.log(reponsedata);
-      alert('User has been registered successfully.');
-      window.open('/client', '_self');
-    })
-    .catch((e) => {
-      console.log(e.response);
-      alert('Error on request');
-    });
-};
+  // do I have to wrap 'render() {}' around the 'return()' ?
+  return (
+    <Container className="container-box">
+      <Form>
+        <Form.Group controlId="formBasicUsername">
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Firtstname blank Lastname"
+            value={username}
+            onChange={(e) => createUsername(e.target.value)}
+          />
+          <Form.Text className="text-muted">
+            We'll never share your personal data with anyone else.
+          </Form.Text>
+        </Form.Group>
 
-// Instead of this return function a registration form is needed (=> install bootstrapReact first)
-return (
-  <form>
-    <label>
-      Username:
-        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-    </label>
-    <label>
-      Password:
-        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-    </label>
-    <button type="button" onClick={handleSubmit}>Submit</button>
-  </form>
-);
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Enter password (8 digits letters and numbers)"
+            value={password}
+            onChange={(e) => createPassword(e.target.value)}
+          />
+        </Form.Group>
 
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Your Email address"
+            value={email}
+            onChange={(e) => createEmail(e.target.value)}
+          />
+        </Form.Group>
 
-RegistrationView.propTypes = {  // why RegistrationView is yellow whereas in other components it's cyan?
-  user: PropTypes.shape({
-    Username: PropTypes.string,
-    Password: PropTypes.string.isRequired,
-    Email: PropTypes.string.isRequired,
-    Birthdate: PropTypes.string.isRequired // is this really a string or rather a date?
-  }).isRequired,
-  onClick: PropTypes.func.isRequired
-};
+        <Form.Group controlId="formBasicBirthdate">
+          <Form.Label>Birthdate</Form.Label>
+          <Form.Control
+            type="date"
+            placeholder="Your day of birth (dd.mm.yyyy)"
+            value={birthdate}
+            onChange={(e) => createBirthdate(e.target.value)}
+          />
+        </Form.Group>
+
+        <Button className="button-primary" variant="primary" type="submit" onClick={this.handleSubmit}>
+          SIGN UP
+        </Button>
+        <Link to={'/login'}>
+          <Button className="button-secondary">LOGIN</Button>
+        </Link>
+      </Form>
+    </Container>
+  );
+}
