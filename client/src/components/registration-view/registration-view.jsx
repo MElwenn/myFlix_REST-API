bcb5c4253056b2dc1a99a0283f12e5eea03d74cc
MElwenn <63125829+MElwenn@users.noreply.github.com>
2020-10-28 11:28:58 +0100

    added createdUser to registration-view.jsx

--- client/src/components/registration-view/registration-view.jsx
+++ client/src/components/registration-view/registration-view.jsx
@@ -16,16 +16,20 @@ export function RegistrationView() {  //function component with hooks
   const handleSubmit = (e) => {  //Send a request to the server for authentication
     e.preventDefault();
 
+    const createdUser = {
+      Username: username,
+      Password: password,
+      Email: email,
+      Birthdate: birthdate
+    };
+
     axios
-      .post('<https://movie-api-elwen.herokuapp.com/users>', {
-        Username: username,
-        Password: password,
-        Email: email,
-        Birthdate: birthdate
-      })
+      .post('<https://movie-api-elwen.herokuapp.com/users>', createdUser)
       .then((response) => {
+        //console.log(response);
+        //console.log(response.data);
         const data = response.data;
-        alert('Your sign up was successful, please login.');
+        alert('You signed up successfully, please login.');
         console.log(data);
         window.open('/client', '_self');
       })
@@ -33,61 +37,62 @@ export function RegistrationView() {  //function component with hooks
         console.log('Error. Your sign up was not successful.');
       });
   };
-};
 
-// do I have to wrap 'render() {}' around the 'return()' ?
-return (
-  <Container className="container-box">
-    <Form>
-      <Form.Group controlId="formBasicUsername">
-        <Form.Label>Username</Form.Label>
-        <Form.Control
-          type="text"
-          placeholder="Firtstname blank Lastname"
-          value={username}
-          onChange={(e) => createUsername(e.target.value)}
-        />
-        <Form.Text className="text-muted">
-          We'll never share your personal data with anyone else.
+
+  // do I have to wrap 'render() {}' around the 'return()' ?
+  return (
+    <Container className="container-box">
+      <Form>
+        <Form.Group controlId="formBasicUsername">
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Firtstname blank Lastname"
+            value={username}
+            onChange={(e) => createUsername(e.target.value)}
+          />
+          <Form.Text className="text-muted">
+            We'll never share your personal data with anyone else.
           </Form.Text>
-      </Form.Group>
+        </Form.Group>
 
-      <Form.Group controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control
-          type="password"
-          placeholder="Enter password (8 digits letters and numbers)"
-          value={password}
-          onChange={(e) => createPassword(e.target.value)}
-        />
-      </Form.Group>
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Enter password (8 digits letters and numbers)"
+            value={password}
+            onChange={(e) => createPassword(e.target.value)}
+          />
+        </Form.Group>
 
-      <Form.Group controlId="formBasicEmail">
-        <Form.Label>Email</Form.Label>
-        <Form.Control
-          type="email"
-          placeholder="Your Email address"
-          value={email}
-          onChange={(e) => createEmail(e.target.value)}
-        />
-      </Form.Group>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Your Email address"
+            value={email}
+            onChange={(e) => createEmail(e.target.value)}
+          />
+        </Form.Group>
 
-      <Form.Group controlId="formBasicBirthdate">
-        <Form.Label>Birthdate</Form.Label>
-        <Form.Control
-          type="date"
-          placeholder="Your day of birth (dd.mm.yyyy)"
-          value={birthdate}
-          onChange={(e) => createBirthdate(e.target.value)}
-        />
-      </Form.Group>
+        <Form.Group controlId="formBasicBirthdate">
+          <Form.Label>Birthdate</Form.Label>
+          <Form.Control
+            type="date"
+            placeholder="Your day of birth (dd.mm.yyyy)"
+            value={birthdate}
+            onChange={(e) => createBirthdate(e.target.value)}
+          />
+        </Form.Group>
 
-      <Button className="button-primary" variant="primary" type="submit" onClick={this.handleSubmit}>
-        SIGN UP
+        <Button className="button-primary" variant="primary" type="submit" onClick={this.handleSubmit}>
+          SIGN UP
         </Button>
-      <Link to={'/login'}>
-        <Button className="button-secondary">LOGIN</Button>
-      </Link>
-    </Form>
-  </Container>
-);
+        <Link to={'/login'}>
+          <Button className="button-secondary">LOGIN</Button>
+        </Link>
+      </Form>
+    </Container>
+  );
+};
